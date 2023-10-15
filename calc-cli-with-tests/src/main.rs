b6a90c5b508bc48571ceebcc110cf92b19a11c50
747hzx <59676354+0xhzx@users.noreply.github.com>
2023-10-15 20:05:23 +0000

    add new args

--- calc-cli-with-tests/src/main.rs
+++ calc-cli-with-tests/src/main.rs
@@ -2,6 +2,8 @@
 
 use calc_cli_with_tests::get_fruits;
 use clap::Parser;
+use std::fs::File;
+use std::io::Write;
 
 /// CLI tool to return random fruits
 #[derive(Parser, Debug)]
@@ -10,10 +12,29 @@ struct Args {
     /// The quantity of fruits to return
     #[clap(short, long, default_value = "1")]
     count: u32,
+    /// The path to the output file
+    #[clap(short, long)]
+    output: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
     let fruits = get_fruits(args.count);
+
     println!("fruits: {:?}", fruits);
+
+    if let Some(output_path) = args.output {
+        // create file automatically
+        if let Ok(mut file) = File::create(output_path.clone()) {
+            for fruit in &fruits {
+                if let Err(err) = writeln!(file, "{}", fruit) {
+                    eprintln!("Failed to write to file: {}", err);
+                    return;
+                }
+            }
+            println!("Output written to file: {}", output_path);
+        } else {
+            eprintln!("Failed to create file: {}", output_path);
+        }
+    }
 }
